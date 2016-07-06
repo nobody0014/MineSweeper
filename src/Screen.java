5ffb0e3289984ca8cd57667d4d38fd6559fb6759
nobody0014 <wittz0014@gmail.com>
2016-07-06 09:38:04 +0700

    1. Add Menu Bar To Do: 1. Figure out how to get 2 pane 2. Seperate Menu Bar from the name of the game? (Maybe)

--- src/Screen.java
+++ src/Screen.java
@@ -13,6 +13,8 @@ public class Screen {
     private JLabel statusLabel;
     private JPanel controlPanel;
     private JPanel gridPanel;
+    private JMenuBar menuBar;
+    private JMenu menu;
     private Cell buttons[];
     private final int space = 3;
 
@@ -21,30 +23,38 @@ public class Screen {
     public void makeScreen(int x, int y){
         Toolkit tk =  Toolkit.getDefaultToolkit();
         Dimension dim = tk.getScreenSize();
-
+        menuBar = new JMenuBar();
+        menuBar.setBorderPainted(true);
+        menu = new JMenu("A Menu");
+        menuBar.add(menu);
         mainFrame = new JFrame("Main");
 
         //Add constant to Y dimension to give area for restart, timer, and number of bombs
-        mainFrame.setSize(x * 35, y * 35 + 50);
+        mainFrame.setSize(x * 40, y * 40 + 50);
 
         //Get X and Y locations where you can start the screen in the middle
         int midX =  dim.width/2 - mainFrame.getWidth()/2;
         int midY = dim.height/2 - mainFrame.getHeight()/2;
-
+        mainFrame.setJMenuBar(menuBar);
         mainFrame.setLocation(midX,midY);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setResizable(true);
+        mainFrame.setResizable(false);
+        //Make proper grid Panel according to the size of the game
         makeGrid(x,y);
         mainFrame.add(gridPanel);
-
-
         mainFrame.setVisible(true);
     }
-
     public void makeGrid(int x, int y){
+        int gridSize;
+        if(x > y){
+            gridSize = y;
+        }
+        else{
+            gridSize = x;
+        }
         buttons = new Cell[x*y];
         gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(x,y));
+        gridPanel.setLayout(new GridLayout(gridSize,gridSize));
         for(int i = 0; i < x*y; i++){
             buttons[i] =  new Cell();
             gridPanel.add(buttons[i]);
