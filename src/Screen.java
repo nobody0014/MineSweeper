beb1af44cff618bfdea89e01c346410644e1f3c5
nobody0014 <wittz0014@gmail.com>
2016-07-06 17:34:45 +0700

    1. Change all the panel to use GridByBagsLayer 2. Change the orientation TODO: 1. Get the buttons to be of bigger size 2. start making proper inforamtion 3. start making proper classes 4. start game logic

--- src/Screen.java
+++ src/Screen.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by wit on 7/5/2016.
@@ -10,12 +12,16 @@ public class Screen {
 
     private JFrame mainFrame;
     private Container controlContainer;
-    private JPanel gridPanel;
+    private JPanel gamePanel;
     private JPanel infoPanel;
+    private GridBagLayout controlLayout;
+    private GridBagLayout gameLayout;
+    private GridBagConstraints gridContraints;
+    private GridBagConstraints infoConstraint;
+    private GridBagConstraints gameConstraint;
     private JMenuBar menuBar;
     private JMenu menu;
     private Cell buttons[];
-    private final int space = 3;
 
 
 
@@ -29,47 +35,72 @@ public class Screen {
         mainFrame = new JFrame("Main");
 
         //Add constant to Y dimension to give area for restart, timer, and number of bombs
-        mainFrame.setSize(x * 40, y * 40 + 100);
-
+        //mainFrame.setSize(x * 40 + 100, y * 40 + 100);
+        mainFrame.setSize(200 + x * 40 ,600);
         //Get X and Y locations where you can start the screen in the middle
         int midX =  dim.width/2 - mainFrame.getWidth()/2;
         int midY = dim.height/2 - mainFrame.getHeight()/2;
         mainFrame.setJMenuBar(menuBar);
+
+        //
         mainFrame.setLocation(midX,midY);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setResizable(false);
 
         //Make Information Area
-        infoPanel = new JPanel();
-        infoPanel.setLayout(new FlowLayout());
-        infoPanel.setSize(x*40, 100);
 
         //Make proper grid Panel according to the size of the game
         makeGrid(x,y);
 
 
-        controlContainer = new Container();
-        controlContainer.setLayout(new GridLayout(2,1));
-        controlContainer.add(infoPanel);
-        controlContainer.add(gridPanel);
-        mainFrame.add(controlContainer);
+        controlContainer = mainFrame.getContentPane();
+        controlContainer.setSize(mainFrame.getWidth(),mainFrame.getHeight());
+        controlLayout = new GridBagLayout();
+        controlContainer.setLayout(controlLayout);
+
+        infoPanel = new JPanel();
+        infoPanel.add(new JLabel("h"));
+        infoConstraint = new GridBagConstraints();
+        infoConstraint.gridx = 0;
+        infoConstraint.gridy = 0;
+        infoConstraint.ipadx = 100;
+        infoConstraint.ipady = 600;
+        infoConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+        controlContainer.add(infoPanel,infoConstraint);
+
+        gameConstraint = new GridBagConstraints();
+        gameConstraint.fill = GridBagConstraints.BOTH;
+        gameConstraint.gridx = 1;
+        gameConstraint.gridy = 0;
+        gameConstraint.ipadx = x*40 + 100;
+        gameConstraint.ipady = 600;
+        gameConstraint.anchor = GridBagConstraints.FIRST_LINE_END;
+        controlContainer.add(gamePanel);
+
+
         mainFrame.setVisible(true);
+
     }
+
+
     public void makeGrid(int x, int y){
-        int gridSize;
-        if(x > y){
-            gridSize = y;
-        }
-        else{
-            gridSize = x;
-        }
+        gamePanel = new JPanel();
+        gameLayout = new GridBagLayout();
+        gridContraints = new GridBagConstraints();
+        gamePanel.setLayout(gameLayout);
         buttons = new Cell[x*y];
-        gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(gridSize,gridSize));
-        for(int i = 0; i < x*y; i++){
-            buttons[i] =  new Cell();
-            gridPanel.add(buttons[i]);
+        int k = 0;
+        for(int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++){
+                buttons[k] = new Cell();
+                gridContraints.fill = GridBagConstraints.BOTH;
+                gridContraints.gridy = i;
+                gridContraints.gridx = j;
+                gamePanel.add(buttons[k],gridContraints);
+                k++;
+            }
         }
+
     }
 
 }
